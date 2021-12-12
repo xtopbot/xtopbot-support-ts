1418bc0c3bb2dc12b91411b0fcee2ad1e537f408
vFaisal <vbnm_0vbnm_0@hotmail.com>
2021-12-13 01:48:45 +0300

    Create Exception.ts

---
+++ src/utils/Exception.ts
@@ -0,0 +1,26 @@
+import FinalResponse from "./FinalResponse";
+import { FinalResponseCode } from "./FinalResponse";
+import { Util } from "discord.js";
+import Logger from "./Logger";
+export default class Exception extends FinalResponse {
+  public message: string;
+  public severity: Severity;
+  public cause?: Error;
+  constructor(message: string, severity: Severity, cause?: Error) {
+    super(FinalResponseCode.EXCEPTION, {
+      content: `Error: \`${Util.escapeMarkdown(message)}\``,
+    }); // this Reply message is temp while locale is finished
+    this.message = message;
+    this.severity = severity;
+    this.cause ??= cause;
+    if (this.severity === Severity.FAULT)
+      Logger.error(`[${this.severity}] ${message}`);
+    else Logger.debug(`[${this.severity}] ${message}`);
+  }
+}
+
+export enum Severity {
+  COMMON,
+  SUSPICIOUS,
+  FAULT,
+}
