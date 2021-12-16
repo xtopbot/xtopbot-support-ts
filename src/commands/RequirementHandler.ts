44bd7df773a6fa2b29d28986fb3e638b1fbaecf9
vFaisal <vbnm_0vbnm_0@hotmail.com>
2021-12-16 21:34:40 +0300

    Add Command Requirements Handler (Part 2)

--- src/commands/RequirementHandler.ts
+++ src/commands/RequirementHandler.ts
@@ -5,7 +5,6 @@ import {
   TextChannel,
   ThreadChannel,
 } from "discord.js";
-import app from "../app";
 import User from "../structures/User";
 import Constants from "../utils/Constants";
 import Exception, { Severity } from "../utils/Exception";
@@ -31,7 +30,7 @@ export default class CommandRequirementsHandler {
     this.user = user;
   }
 
-  public all(): FinalResponse | boolean {
+  public checkAll(): FinalResponse | boolean {
     if (!this.userLevelPolicy())
       return new FinalResponse(
         FinalResponseCode.UNAUTHORIZED_USER_LEVEL_POLICY,
@@ -39,35 +38,58 @@ export default class CommandRequirementsHandler {
           content: "Unauthorized user level policy",
         }
       );
+
     if (!this.checkBotChannelPermissions())
       return new FinalResponse(
         FinalResponseCode.BOT_CHANNEL_PERMISSIONS_MISSING,
         {
-          content: `The bot permissions in this channel are missing. Please check \`${Util.permissionsToStringArray(
+          content: `The bot permissions for this channel are missing. Please check \`${Util.permissionsToStringArray(
             this.botChannelPermissionsMissing
           ).join(", ")}\`.`,
         }
       );
+
     if (!this.checkBotGuildPermissions())
       return new FinalResponse(
         FinalResponseCode.BOT_GUILD_PERMISSIONS_MISSING,
         {
-          content: `The bot permissions in this guild are missing. Please check \`${Util.permissionsToStringArray(
+          content: `The bot permissions for this guild are missing. Please check \`${Util.permissionsToStringArray(
             this.botGuildPermissionsMissing
           ).join(", ")}\`.`,
+        }
+      );
+
+    if (!this.checkMemberChannelPermissions())
+      return new FinalResponse(
+        FinalResponseCode.BOT_CHANNEL_PERMISSIONS_MISSING,
+        {
+          content: `Member permissions for this channel are missing. Please check \`${Util.permissionsToStringArray(
+            this.memberChannelPermissionsMissing
+          ).join(", ")}\` **(requires only one of the permissions listed)**.`,
+        }
+      );
+
+    if (!this.checkMemberGuildPermissions())
+      return new FinalResponse(
+        FinalResponseCode.MEMBER_GUILD_PERMISSIONS_MISSING,
+        {
+          content: `Member permissions for this guild are missing. Please check \`${Util.permissionsToStringArray(
+            this.memberGuildPermissionsMissing
+          ).join(", ")}\` **(requires only one of the permissions listed)**.`,
         }
       );
     return true;
   }
 
   public userLevelPolicy(): boolean {
-    return false;
+    if (this.command.level > this.user.levelPolicy) return false;
+    return true;
   }
 
   private get me(): GuildMember {
     if (!this.channel.guild.me)
       throw new Exception(
-        "[CommandRequirementsHandler] (me) field is not object.",
+        `[${this.constructor.name}] (me) field is not object cannot handler this.`,
         Severity.FAULT
       );
     return this.channel.guild.me;
@@ -85,14 +107,12 @@ export default class CommandRequirementsHandler {
 
   private get botChannelPermissionsMissing(): Array<PermissionString> {
     return this.channel
-      .permissionsFor(this.member.guild.me as GuildMember)
+      .permissionsFor(this.me)
       .missing(this.botChannelPermissions);
   }
 
   public checkBotChannelPermissions(): boolean {
-    return this.channel
-      .permissionsFor(this.member.guild.me)
-      .has(this.botChannelPermissions);
+    return this.channel.permissionsFor(this.me).has(this.botChannelPermissions);
   }
 
   /**
@@ -106,12 +126,50 @@ export default class CommandRequirementsHandler {
   }
 
   private get botGuildPermissionsMissing(): Array<PermissionString> {
-    return this.channel.guild.me.permissions.missing(this.botGuildPermissions);
+    return this.me.permissions.missing(this.botGuildPermissions);
   }
 
   public checkBotGuildPermissions(): boolean {
+    return this.me.permissions.has(this.botGuildPermissions);
+  }
+
+  /**
+   * Member Channel Permissions (Requires one of permissions)
+   */
+
+  private get memberChannelPermissions(): Array<PermissionString> {
+    return this.command.memberPermissions.filter((permission) =>
+      Constants.CHANNEL_PERMISSIONS.includes(permission)
+    );
+  }
+
+  private get memberChannelPermissionsMissing(): Array<PermissionString> {
     return this.channel
-      .permissionsFor(this.member.guild.me as GuildMember)
-      .has(this.botGuildPermissions);
+      .permissionsFor(this.member)
+      .missing(this.memberChannelPermissions);
+  }
+
+  public checkMemberChannelPermissions(): boolean {
+    return this.channel
+      .permissionsFor(this.member)
+      .any(this.memberChannelPermissions);
+  }
+
+  /**
+   * Member Guild Permissions (Requires one of permissions)
+   */
+
+  private get memberGuildPermissions(): Array<PermissionString> {
+    return this.command.memberPermissions.filter(
+      (permission) => !Constants.CHANNEL_PERMISSIONS.includes(permission)
+    );
+  }
+
+  private get memberGuildPermissionsMissing(): Array<PermissionString> {
+    return this.member.permissions.missing(this.memberGuildPermissions);
+  }
+
+  public checkMemberGuildPermissions(): boolean {
+    return this.member.permissions.any(this.memberGuildPermissions);
   }
 }
