65c4dd58b05a0691989d81924e439d7d706239f5
vFaisal <vbnm_0vbnm_0@hotmail.com>
2022-01-01 14:18:18 +0300

    Update MessageCreate.ts

--- src/listeners/MessageCreate.ts
+++ src/listeners/MessageCreate.ts
@@ -1,12 +1,19 @@
 import { Message } from "discord.js";
 import CommandHandler from "../commands/CommandHandler";
+import Logger from "../utils/Logger";
 
 export default class MessageCreate {
   public static async onMessageCreate(message: Message) {
     try {
       await CommandHandler.onCommand(message);
     } catch (err) {
-      console.log(err);
+      Logger.error(
+        `[App](Event: ${this.constructor.name}) Error while execute: ${
+          (err as Error).message
+        }`
+      );
+
+      console.error(err);
     }
   }
 }
