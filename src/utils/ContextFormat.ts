f7e76ecb62855c90dec628961888d958cf5fb0c8
Faisal <vbnm_0vbnm_0@hotmail.com>
2022-02-25 19:21:09 +0300

    ContextFormats

--- src/utils/ContextFormat.ts
+++
@@ -1,7 +0,0 @@
-import { Collection } from "discord.js";
-import Locale from "../structures/Locale";
-
-export default class ContextFormat {
-  public formats: Collection<string, string | number> = new Collection();
-  constructor() {}
-}
---
+++ src/utils/ContextFormats.ts
@@ -0,0 +1,61 @@
+import { Collection } from "discord.js";
+import Response from "./Response";
+
+export default class ContextFormats {
+  public readonly formats: Collection<string, string> = new Collection();
+
+  public setObject(keyOrigin: string, input: Object): void {
+    for (let [key, value] of Object.entries({ ...input })) {
+      let _key = keyOrigin + "." + key;
+      if (typeof value == "object" && value !== null)
+        this.setObject(_key, value);
+      else if (typeof value == "string" || typeof value == "number")
+        this.formats.set(_key, String(value));
+      else if (typeof value == "boolean")
+        this.formats.set(_key, value ? "True" : "False");
+      else this.formats.set(_key, "(Unknown format)");
+    }
+  }
+
+  public resolve(response: Response): any {
+    return this.resolveObject(response.message);
+  }
+
+  private resolveObject(input: any): any {
+    if (typeof input == "string") return this.resloveString(input);
+    if (typeof input !== "object") return input;
+    let obj = { ...input };
+
+    for (let [key, value] of Object.entries(obj)) {
+      if (typeof value == "object") {
+        obj[key] = Array.isArray(value)
+          ? value.map((_v) => this.resolveObject(_v))
+          : this.resolveObject(value);
+      } else if (typeof value == "string") {
+        obj[key] = this.resloveString(value);
+      }
+    }
+
+    return obj;
+  }
+
+  private get RegexFormats(): RegExp {
+    return new RegExp(
+      this.formats
+        .map((_value, key) =>
+          `{{${key}}}`.replace(
+            /\[|\]|\^|\*|\{|\}|\(|\)|\./gi,
+            (match) => `\\${match}`
+          )
+        )
+        .join("|"),
+      "gi"
+    );
+  }
+
+  private resloveString(input: string): string {
+    return input.replace(this.RegexFormats, (match) => {
+      return this.formats.get(match.slice(2, -2)) ?? match ?? "";
+    });
+  }
+}
