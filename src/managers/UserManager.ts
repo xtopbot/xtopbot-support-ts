2ca9951b7ff625c551c30d8e8df92b7d07c4fa16
Faisal <vbnm_0vbnm_0@hotmail.com>
2022-05-18 17:25:32 +0300

    Improve UserManager

--- src/managers/UserManager.ts
+++ src/managers/UserManager.ts
@@ -1,54 +1,60 @@
 import User, { UserFlagsPolicy } from "../structures/User";
-import { Collection, User as DiscordUser } from "discord.js";
+import { User as DiscordUser } from "discord.js";
 import CacheManager from "./CacheManager";
 import db from "../providers/Mysql";
 import app from "../app";
 import Exception, { Severity } from "../utils/Exception";
+import Logger from "../utils/Logger";
 export default class UserManager extends CacheManager<User> {
   constructor() {
     super();
   }
 
-  private async _fetch(userId: string): Promise<DiscordUser> {
-    try {
-      const user = app.client.users.fetch(userId);
-      return user;
-    } catch (err) {
-      throw new Exception((err as Error).message, Severity.FAULT, err as Error);
-    }
-  }
-
   public async fetch(
-    _user: DiscordUser | string,
+    user: DiscordUser | string,
     force: boolean = true
   ): Promise<User> {
-    const user: DiscordUser =
-      _user instanceof DiscordUser ? _user : await this._fetch(_user);
+    user = await this.resolveDiscordUser(user);
     if (!force) {
       const cached = this.cache.get(user.id);
       if (cached instanceof User) return cached;
     }
-
     const raw = await db.query(
       "SELECT userId, SUM(flags) as flags, locale, createdAt FROM `Users` WHERE userId = ?",
       [user.id]
     );
     if (!raw[0].userId) {
-      console.log("User not found");
       await this.create(user);
-      return this.fetch(_user, true);
+      return this.fetch(user, true);
     }
     return this._add(new User(raw[0]));
   }
 
-  async create(_user: DiscordUser | string, locale?: string): Promise<void> {
-    const _locale = locale ?? null;
-    const user: DiscordUser =
-      _user instanceof DiscordUser ? _user : await this._fetch(_user);
+  public async create(
+    user: DiscordUser | string,
+    locale?: string | null
+  ): Promise<void> {
+    user = await this.resolveDiscordUser(user);
+    Logger.info(`Create ${user.tag}<${user.id}> in datebase.`);
+    locale = locale ?? null;
     await db.query(
       "INSERT INTO `Users` SET userId = ?, locale = ? ON DUPLICATE KEY UPDATE `locale` = CASE ? WHEN null THEN `locale` ELSE ? END;",
-      [user.id, _locale, _locale, _locale]
+      [user.id, locale, locale, locale]
     );
+  }
+
+  private async resolveDiscordUser(
+    _user: DiscordUser | string
+  ): Promise<DiscordUser> {
+    return _user instanceof DiscordUser
+      ? _user
+      : await app.client.users.fetch(_user).catch((err) => {
+          throw new Exception(
+            (err as Error).message,
+            Severity.FAULT,
+            err as Error
+          );
+        });
   }
 }
 
