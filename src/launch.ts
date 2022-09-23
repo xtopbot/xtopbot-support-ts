1c6b6e05bf73207d68d56d55f5a28aea6a9cf200
Faisal <15677064+vFaisal@users.noreply.github.com>
2022-09-23 08:14:00 +0300

    Add handle unknown errors

--- src/launch.ts
+++ src/launch.ts
@@ -1,3 +1,15 @@
 import app from "./app";
+import Exception from "./utils/Exception";
 
 app.launch();
+
+//Handle unknown errors
+process.on("uncaughtException", function (err) {
+  // Handle the error safely
+  if (!(err instanceof Exception)) console.error(err);
+});
+
+process.on("unhandledRejection", function (err) {
+  // Handle the error safely
+  if (!(err instanceof Exception)) console.error(err);
+});
