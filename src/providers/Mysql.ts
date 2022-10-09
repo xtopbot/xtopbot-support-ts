e4457b8013fa5c3179c247fde4af324fabc9e7a9
Faisal <15677064+vFaisal@users.noreply.github.com>
2022-10-09 07:45:41 +0300

    Mysql move Connection to Pool

--- src/providers/Mysql.ts
+++ src/providers/Mysql.ts
@@ -2,11 +2,11 @@ import mysql2, { Connection, QueryError } from "mysql2/promise";
 import Exception, { Severity } from "../utils/Exception";
 import Logger from "../utils/Logger";
 import * as dotenv from "dotenv";
-
+import app from "../app";
 dotenv.config();
 
 export default class MysqlDatabase {
-  private static configConnection: mysql2.ConnectionOptions = {
+  private static configConnection: mysql2.PoolOptions = {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
@@ -14,13 +14,13 @@ export default class MysqlDatabase {
     charset: "utf8mb4",
   };
   public static state: MysqlConnectionState;
-  private static connection: Connection;
+  private static connection: mysql2.Pool;
 
-  public static async connect(): Promise<void> {
+  public static async connect() {
     try {
       this.state = MysqlConnectionState.CONNECTING;
       Logger.info("[Mysql] Database connecting...");
-      this.connection = await mysql2.createConnection(this.configConnection);
+      this.connection = await mysql2.createPool(this.configConnection);
       this.state = MysqlConnectionState.CONNECTED;
       Logger.info("[Mysql] Database connected!");
     } catch (err) {
@@ -30,7 +30,7 @@ export default class MysqlDatabase {
     }
   }
 
-  public static get db(): Connection {
+  public static get db() {
     if (this.state !== MysqlConnectionState.CONNECTED)
       throw Error("Cannot use db while not connected.");
     return this.connection;
@@ -39,13 +39,14 @@ export default class MysqlDatabase {
   public static async query(
     query: string,
     values?: Array<any> | object
-  ): Promise<any | Exception> {
-    //Logger.debug(`[Mysql<Query>] ${query} [${values?.toString() ?? ""}]`);
+  ): Promise<any> {
+    if (app.mode == "DEV")
+      Logger.info(`[Mysql<Query>] ${query} [${values?.toString() ?? ""}]`);
     try {
       const [raw] = await this.db.query(query, values);
       return raw;
-    } catch (err) {
-      throw new Exception((err as QueryError).message, Severity.FAULT, err);
+    } catch (err: QueryError | any) {
+      throw new Exception(err?.message, Severity.FAULT, err);
     }
   }
 }
@@ -55,5 +56,3 @@ enum MysqlConnectionState {
   CONNECTING,
   CONNECTED,
 }
-
-//export const db: Connection = MysqlDatabase.db;
